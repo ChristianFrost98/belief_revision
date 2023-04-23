830b1374cc65131635ff9efd3a9c55feb8b1103e
s184140 <s184140@student.dtu.dk>
2023-04-23 11:41:06 +0200

    feat(Data model): first iteration

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 """
 1 ) Belief base implementation
 - Lav en klasse som kan afbilde en belief base og dets set af beliefs
@@ -6,6 +8,47 @@
 - Det skal være muligt at printe denne belief base
 - Det skal være muligt at intere denne belief base
 """
+
+
+class Opr(Enum):
+    AND = "AND"
+    OR = "OR"
+    NOT = "NOT"
+    IMPLIES = "->"
+    BIIMPLIES = "<->"
+
+    def __repr__(self):
+        return " " + str(self.value) + " "
+
+
+class Belief:
+    def __init__(self, belief, priority):
+        self.belief = belief
+        self.priority = priority
+
+    def __repr__(self):
+        res = ""
+        for elm in self.belief:
+            res += repr(elm)
+        return "(" + res + ")"
+
+def test():
+    belief_base = [
+        Belief(belief=["A", Opr.OR, Belief(belief=["A", Opr.BIIMPLIES, "B"], priority=2)], priority=2),
+        Belief(belief=["A", Opr.AND, "C"], priority=1),
+        Belief(belief=["A"], priority=1),
+        Belief(belief=[Opr.NOT, "B"], priority=1),
+        Belief(belief=["A", Opr.IMPLIES, "B"], priority=1),
+        Belief(belief=["Q", Opr.OR, "R", Opr.OR, Opr.NOT, "Q", Opr.OR, "S"], priority=1)
+    ]
+
+    for x in belief_base:
+        print(x)
+
+
+
+if __name__ == "__main__":
+    test()
 
 
 
@@ -17,8 +60,6 @@
 - Der skal implementeres en funktion til resolution for at tjekke entailment
 """
 
-
-
 """
 3 ) Implementation of contraction of belief base:
 - Her ønsker vi at tilføje et nyt belief til vores belief base, derfor skal
@@ -27,8 +68,6 @@ skal resolution funktion til at tjekke om det nye belief passer ind.
 - Her skal der benyttes AGM posulater til at tjekke om contraction var
 en success
 """
-
-
 
 """
 4 ) Implementation of expansion of belief base:
