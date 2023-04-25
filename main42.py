c5b348e0234420dfb01b3b44a31d0bd97e5b2cb7
s184140 <s184140@student.dtu.dk>
2023-04-25 09:57:08 +0200

    feat(&): wtf

--- main42.py
+++ main42.py
@@ -7,11 +7,12 @@ from sympy.parsing.sympy_parser import parse_expr
 # Initialize the belief base as an empty list
 belief_base = []
 highest_priority = 0
+
+
 class BeliefBase:
-    
+
     def __init__(self, belief_base):
         self.belief_base = belief_base
-        
 
     # Add a new formula to the belief base with a priority
     def add_formula(self, formula, priority):
@@ -21,19 +22,19 @@ class BeliefBase:
             highest_priority = priority
 
     # Remove a formula from the belief base
-    def remove_formula(self,formula):
+    def remove_formula(self, formula):
         self.belief_base[:] = [f for f in self.belief_base if f[0] != formula]
 
     # Check if a formula is in the belief base
-    def contains_formula(self,formula):
+    def contains_formula(self, formula):
         return any(f[0] == formula for f in self.belief_base)
 
     # Get the priority of a formula in the belief base
-    def get_priority(self,formula):
+    def get_priority(self, formula):
         return next((f[1] for f in self.belief_base if f[0] == formula), None)
 
     # Update the priority of a formula in the belief base
-    def update_priority(self,formula, new_priority):
+    def update_priority(self, formula, new_priority):
         global highest_priority
         for i, f in enumerate(self.belief_base):
             if f[0] == formula:
@@ -46,57 +47,44 @@ class BeliefBase:
     def get_sorted_formulas(self):
         return [f[0] for f in sorted(self.belief_base, key=lambda x: x[1])]
 
-def convert_to_cnf(belief_base):
-    
+
+def convert_to_cnf(bb):
     new_belief_base = BeliefBase([])
-    
-    for (belief,priority) in belief_base.belief_base:
-        #belief = b[0]
-        #priority = b[1]
+
+    for (belief, priority) in bb.belief_base:
+        # Define symbols for all variables in the expression
         variables = set(filter(str.isalpha, belief))
         symbols = {v: Symbol(v) for v in variables}
+
         # Replace variables in the expression string with corresponding symbols
         for v, s in symbols.items():
             belief = belief.replace(v, str(s))
-        
-        belief = sympy.sympify(belief)
-            
-        new_belief_base.add_formula(belief, priority)
 
+        # Parse the expression string into a Sympy expression
+        new_belief_base.add_formula(sympy.sympify(belief), priority)
 
-    
     return new_belief_base
-    
 
-
+"""
 def contraction(belief_base, formula):
     # If the new formula is already in the belief base, return the belief base
     if contains_formula(formula):
         return belief_base
-    
-    
+
     cnf_string = convert_to_CNF(new_belief_base)
-    
-    
-    
+
     # Add the new formula to the belief base
     add_formula(formula, highest_priority + 1)
-    
-
-
-    
-        
-        
+"""
 
 def test():
-    
     belief_base = BeliefBase([])
-    
+
     # Add beliefs to the belief base
-    belief_base.add_formula('P', 1)    # Priority 1
-    belief_base.add_formula('Q', 2)    # Priority 2
-    belief_base.add_formula('P & Q', 3)    # Priority 3
-    belief_base.add_formula('~P | R', 4)   # Priority 4
+    belief_base.add_formula("P", 1)  # Priority 1
+    belief_base.add_formula("Q", 2)  # Priority 2
+    belief_base.add_formula("P | D & C", 3)  # Priority 3
+    belief_base.add_formula("~P & R", 4)  # Priority 4
 
     # Get all beliefs in the belief base
     beliefs = belief_base.get_sorted_formulas()
@@ -124,16 +112,13 @@ def test():
     
     print(belief_base)
     """
-    #print(convert_to_cnf(belief_base))
-    #print(is_cnf(q | p | r))
-    #print(to_cnf(~(A | B) | D))
-    #print(parse_expr('p | q | r'))
-    
-    
+    # print(convert_to_cnf(belief_base))
+    # print(is_cnf(q | p | r))
+    # print(to_cnf(~(A | B) | D))
+    # print(parse_expr('p | q | r'))
+
     print(convert_to_cnf(belief_base))
-    
-    
+
 
 if __name__ == "__main__":
     test()
-
